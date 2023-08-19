289c77e309ec23618a4037d4968c169a306dc1cc
NekoNekoNyan <me@neko-dev.ru>
2023-08-19 17:00:10 +0300

    fix compiler warning

--- src/macro.hpp
+++ src/macro.hpp
@@ -4,7 +4,7 @@
 #define VSPTR const void*
 #define CSREF const String&
 #define GH_PGM(name, str) static const char name[] PROGMEM = str
-#define GH_PGM_LIST(name, ...) const char* const name[] PROGMEM = {__VA_ARGS__};
+#define GH_PGM_LIST(name, ...) const char* const name[] PROGMEM = {__VA_ARGS__}
 #define FSTR const __FlashStringHelper*
 #define GH_NO_LABEL F("_no")
 #define GH_NUMBERS F("^\\d+$")
